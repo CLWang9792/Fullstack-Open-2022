58d2d9792f151f3a4a807607bdcefb4875f18661
CLWang <clwang@CLWangdeMacBook-Air.local>
2022-07-10 04:14:53 +0800

    part 2.1~2.5

---
+++ part2/2.1~2.5/src/App.js
@@ -0,0 +1,12 @@
+import Course from "./components/Note"
+
+const App = ({ course }) => {
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/2.1~2.5/src/components/Note.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Header = ({ tital }) => {
+  return (
+    <div>
+      <h1>{tital}</h1>
+    </div>
+  )
+}
+
+const SubHeader = ({ course }) => {
+  return (
+    <div>
+      <h2>{course.name}</h2>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+      <p>
+        {part.name} {part.exercises}
+      </p>
+  )
+}
+
+const Content = ({ part }) => {
+  return (
+    <div>
+      {part.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = ({ part }) => {
+  const total = part.map(item => item.exercises)
+                    .reduce((a, b) => a + b)
+  return (
+    <p>total of {total} exercises</p>
+  ) 
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header tital={"Web development curriculum"} />
+      {course.map(course => 
+      <>
+        <SubHeader key={course.id} course={course} />
+        <Content key={course.id} part={course.part} />
+        <Total key={course.id} part={course.part} />
+      </>
+      )}
+    </div>
+  )
+}
+
+export default Course
